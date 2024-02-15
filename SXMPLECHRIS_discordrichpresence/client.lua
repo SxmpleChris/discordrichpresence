2b1a095a42adad1b924b3a99a00d1250619eb8e2
SxmpleChris <159816650+SxmpleChris@users.noreply.github.com>
2024-02-15 22:08:49 +0100

    Update client.lua

--- SXMPLECHRIS_discordrichpresence/client.lua
+++ SXMPLECHRIS_discordrichpresence/client.lua
@@ -2,22 +2,22 @@ Citizen.CreateThread(function()
     while true do
         local PlayerName = GetPlayerName(PlayerId())
         local id = GetPlayerServerId(PlayerId())
-        SetDiscordAppId(1202270637868208208)
-        SetRichPresence(PlayerName.." | ID: "..id.."") -- This will take the player name and the Id
+        SetDiscordAppId(0000000000000) -- Replace all Zero's with your Application ID (https://discord.com/developers)
+        SetRichPresence(PlayerName.." | ID: "..id.."") -- This defines the players name and his ID, I suggest you do not change that.
         -- Here you will have to put the image name for the "large" icon.
         -- You can create one by go to Rich Presence/Art Assets tab in your application and  click Add Image(s)
-        -- The Paramater is your Image key that you uploaded (you can change it too once you upload)
-        SetDiscordRichPresenceAsset('projektdeutschland')
+        -- The Paramater is your Image name that you uploaded (you can change it too once you upload)
+        SetDiscordRichPresenceAsset('EXAMPLEIMAGE')
         -- Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('Projekt Deutschland')
+        SetDiscordRichPresenceAssetText('YOURPROJECT')
         --[[ 
             Here you can add buttons that will display in your Discord Status,
             First paramater is the button index (0 or 1), second is the title and 
             last is the url (this has to start with "fivem://connect/" or "https://") 
         ]]--
-        SetDiscordRichPresenceAction(0, "Beitreten", "fivem://connect/br5mgl")
+        SetDiscordRichPresenceAction(0, "Connect", "fivem://connect/XXXXX")
         --You can add more Natives Here vvv
-        SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/rS57VRsmvk")
+        SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/INVITE")
         -- Updates every 30 seconds
         Citizen.Wait(30000)
     end
